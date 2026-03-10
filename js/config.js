fefeb74e1eca3aaf7175911053b7e246399703b4
Maheshmax1 <maheshmaari2409@gmail.com>
2026-03-10 16:25:07 +0530

    added

--- js/config.js
+++ js/config.js
@@ -27,7 +27,10 @@ function handleAuthError() {
     window.location.href = insidePages ? "../index.html" : "index.html";
 }
 
-
+/**
+ * apiRequest: A simple helper for all database calls.
+ * It automatically adds the 'Bearer' token for you.
+ */
 async function apiRequest(endpoint, method = "GET", body = null, requireAuth = true) {
     const token = getToken();
     const headers = { "Accept": "application/json" };
